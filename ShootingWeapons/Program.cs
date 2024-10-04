101af1bfe04cbb8e395f238f5a65fe19478df9df
BolNick <BolNick09RUS@gmail.com>
2024-10-04 15:41:54 +0300

    Добавьте файлы проекта.

---
+++ ShootingWeapons/Program.cs
@@ -0,0 +1,56 @@
+﻿namespace ShootingWeapons
+{
+    using Weapons;
+
+    internal class Target
+    {
+        private int health;
+
+        public Target(int initialHealth)
+        {
+            health = initialHealth;
+        }
+
+        public void Destroy(IWeapon weapon)
+        {
+            Console.WriteLine($"здоровье мишени: {health}");
+            Console.WriteLine($"УВС оружия: {weapon.Damage * weapon.FireRate}\n");
+            int shotCount = 0;
+            
+            while (health > 0)
+            {
+                shotCount++;
+                health -= weapon.Damage;
+                if (health < 0)
+                    health = 0;
+                Console.WriteLine($"Выстрел {shotCount}. Оставшееся здоровье {health}");
+
+
+                Thread.Sleep((int)(1/ weapon.FireRate * 1000));
+            }
+
+            Console.WriteLine($"Мишень уничтожена. Количество выстрелов: {shotCount}");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Пистолет");
+            Target tgt1 = new(100);
+            tgt1.Destroy(Weapons.Create("Pistol"));
+            Console.WriteLine("------------------------------------");
+            Console.WriteLine("Дробовик");
+            Target tgt2 = new(250);
+            tgt2.Destroy(Weapons.Create("Shotgun"));
+            Console.WriteLine("------------------------------------");
+            Console.WriteLine("Винтовка");
+            Target tgt3 = new(500);
+            tgt3.Destroy(Weapons.Create("Rifle"));
+
+
+
+
+        }
+    }
+}
---
+++ Weapons/Weapons.cs
@@ -0,0 +1,52 @@
+﻿namespace Weapons
+{
+    public interface IWeapon
+    {
+        int Damage { get; }
+        float FireRate { get; }
+    }
+
+    public static class Weapons
+    {
+        public static IWeapon Create(string name)
+        {
+            switch (name)
+            {
+                case "Pistol":
+                {
+                    return new Pistol();
+                }
+                case "Shotgun":
+                {
+                    return new Shotgun();
+                }
+                case "Rifle":
+                {
+                    return new Rifle();
+                }
+                default:
+                {
+                    throw new ArgumentException($"Неизвестное оружие: {name}");
+                }
+            }
+        }
+    }
+
+    internal class Pistol : IWeapon
+    {
+        public int Damage { get; } = 20;
+        public float FireRate { get; } = 1.0f;
+    }
+
+    internal class Shotgun : IWeapon
+    {
+        public int Damage { get; } = 50;
+        public float FireRate { get; } = 0.5f;
+    }
+
+    internal class Rifle : IWeapon
+    {
+        public int Damage { get; } = 30;
+        public float FireRate { get; } = 5.0f;
+    }
+}
